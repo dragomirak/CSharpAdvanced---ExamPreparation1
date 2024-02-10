4d07226dd6eff2331d6b39a3a69cdaa7474e9bd8
Dragomira <132193889+dragomirak@users.noreply.github.com>
2024-02-10 19:06:05 +0200

    Add project files.

---
+++ FishingCompetition/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace FishingCompetition;
+
+public class Program
+{
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        char[,] matrix = new char[size, size];
+        CreateMatrix(matrix);
+
+        int currentRow = 0;
+        int currentCol = 0;
+        int fishCaught = 0;
+        bool quotaReached = false;
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                if (matrix[row, col] == 'S')
+                {
+                    currentRow = row;
+                    currentCol = col;
+                }
+            }
+        }
+
+        string command;
+        while ((command = Console.ReadLine()) != "collect the nets")
+        {
+            matrix[currentRow, currentCol] = '-';
+
+            if (command == "up")
+            {
+                currentRow--;
+                if (currentRow < 0)
+                {
+                    currentRow = matrix.GetLength(0) - 1;
+                }
+            }
+            else if (command == "down")
+            {
+                currentRow++;
+                if (currentRow > matrix.GetLength(0) - 1)
+                {
+                    currentRow = 0;
+                }
+            }
+            else if (command == "left")
+            {
+                currentCol--;
+                if (currentCol < 0)
+                {
+                    currentCol = matrix.GetLength(1) - 1;
+                }
+            }
+            else if (command == "right")
+            {
+                currentCol++;
+                if (currentCol > matrix.GetLength(1) - 1)
+                {
+                    currentCol = 0;
+                }
+            }
+
+            if (matrix[currentRow, currentCol] == '-')
+            {
+                matrix[currentRow, currentCol] = 'S';
+                continue;
+            }
+            else if (matrix[currentRow, currentCol] == 'W')
+            {
+                Console.WriteLine($"You fell into a whirlpool! The ship sank and you lost the fish you caught. " +
+                    $"Last coordinates of the ship: [{currentRow},{currentCol}]");
+                return;
+            }
+            else
+            {
+                fishCaught += int.Parse(matrix[currentRow, currentCol].ToString());
+                if (fishCaught >= 20)
+                {
+                    quotaReached = true;
+                }
+                matrix[currentRow, currentCol] = 'S';
+                continue;
+            }
+        }
+
+        if (quotaReached)
+        {
+            Console.WriteLine("Success! You managed to reach the quota!");
+        }
+        else
+        {
+            Console.WriteLine($"You didn't catch enough fish and didn't reach the quota! " +
+                $"You need {20 - fishCaught} tons of fish more.");
+        }
+
+        if (fishCaught > 0)
+        {
+            Console.WriteLine($"Amount of fish caught: {fishCaught} tons.");
+        }
+
+        PrintMatrix(matrix);
+
+    }
+
+    private static void CreateMatrix(char[,] matrix)
+    {
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            char[] charArray = Console.ReadLine().ToCharArray();
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = charArray[col];
+            }
+        }
+    }
+
+    private static void PrintMatrix(char[,] matrix)
+    {
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                Console.Write(matrix[row, col]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ OffroadChallenge/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace OffroadChallenge;
+
+public class Program
+{
+    static void Main()
+    {
+        Stack<int> initialFuel = new Stack<int>(Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse));
+
+        Queue<int> addConsumption = new Queue<int>(Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse));
+
+        Queue<int> neededFuel = new Queue<int>(Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse));
+
+        int initialStackCount = initialFuel.Count();
+        int altitudeCount = 0;
+        Queue<int> reachedAltitudes = new();
+
+        while (initialFuel.Count > 0)
+        {
+            int result = initialFuel.Pop() - addConsumption.Dequeue();
+            altitudeCount++;
+            if (result >= neededFuel.Dequeue())
+            {
+                Console.WriteLine($"John has reached: Altitude {altitudeCount}");
+                reachedAltitudes.Enqueue(altitudeCount);
+            }
+            else if (result < neededFuel.Dequeue())
+            {
+                Console.WriteLine($"John did not reach: Altitude {altitudeCount}");
+                break;
+            }
+        }
+
+        if (reachedAltitudes.Count == 0)
+        {
+            Console.WriteLine("John failed to reach the top.");
+            Console.WriteLine("John didn't reach any altitude.");
+        }
+        else if (reachedAltitudes.Count > 0 &&
+            reachedAltitudes.Count < initialStackCount)
+        {
+            Console.WriteLine("John failed to reach the top.");
+            Console.Write("Reached altitudes: ");
+            for (int i = 0; i < reachedAltitudes.Count - 1; i++)
+            {
+                Console.Write($"Altitude {reachedAltitudes.Dequeue()}, ");
+            }
+            Console.Write($"Altitude {reachedAltitudes.Dequeue()}");
+        }
+        else if (reachedAltitudes.Count == initialStackCount)
+        {
+            Console.WriteLine("John has reached all the altitudes and managed to reach the top!");
+        }
+    }
+}
